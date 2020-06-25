96a6c25f6e55d82dd7264a27d9f30f0f096abd23
omkar2809 <zemseom10@gmail.com>
2020-06-25 16:51:26 +0530

    create channel bugs fix

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -1,7 +1,7 @@
 
 # chmod -R 0755 ./crypto-config
 # # Delete existing artifacts
-rm -rf ./crypto-config
+# rm -rf ./crypto-config
 # rm genesis.block mychannel.tx
 # rm -rf ../../channel-artifacts/*
 
@@ -10,37 +10,38 @@ rm -rf ./crypto-config
 
 
 
-# # System channel
+# System channel
 SYS_CHANNEL="sys-channel"
 
-# # channel name defaults to "mychannel"
-CHANNEL_NAME1="basicchannel"
-CHANNEL_NAME2="bidchannel"
+# channel name defaults to "mychannel"
+CHANNEL_NAME1="bidchannel"
 
 echo $CHANNEL_NAME1
 
-# # Generate System Genesis block
+# Generate System Genesis block
 ./bin/configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
 
 
-# # # # Generate channel configuration block
-./bin/configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./basicchannel.tx -channelID $CHANNEL_NAME1
+# Generate channel configuration block
+./bin/configtxgen -profile BidChannel -configPath . -outputCreateChannelTx ./bidchannel.tx -channelID $CHANNEL_NAME1
 
-./bin/configtxgen -profile BidChannel -configPath . -outputCreateChannelTx ./bidchannel.tx -channelID $CHANNEL_NAME2
+echo "#######    Generating anchor peer update for Org1MSP  ##########"
+./bin/configtxgen -profile BidChannel -configPath . -outputAnchorPeersUpdate ./BidderMSPanchors_bidchannel.tx -channelID $CHANNEL_NAME1 -asOrg BidderMSP
 
-echo "#######    Generating anchor peer update for GovMSP  ##########"
-./bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./GovMSPanchors_basicchannel.tx -channelID $CHANNEL_NAME1 -asOrg GovMSP
+echo "#######    Generating anchor peer update for Org2MSP  ##########"
+./bin/configtxgen -profile BidChannel -configPath . -outputAnchorPeersUpdate ./GovMSPanchors_bidchannel.tx -channelID $CHANNEL_NAME1 -asOrg GovMSP
 
-echo "#######    Generating anchor peer update for BidderMSP  ##########"
-./bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./BidderMSPanchors_basicchannel.tx -channelID $CHANNEL_NAME1 -asOrg BidderMSP
-
-echo "#######    Generating anchor peer update for BankMSP  ##########"
-./bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./BankMSPanchors_basicchannel.tx -channelID $CHANNEL_NAME1 -asOrg BankMSP
+CHANNEL_NAME2="basicchannel"
 
 echo $CHANNEL_NAME2
 
-echo "#######    Generating anchor peer update for GovMSP  ##########"
-./bin/configtxgen -profile BidChannel -configPath . -outputAnchorPeersUpdate ./GovMSPanchors_bidchannel.tx -channelID $CHANNEL_NAME2 -asOrg GovMSP
+./bin/configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./basicchannel.tx -channelID $CHANNEL_NAME2
 
-echo "#######    Generating anchor peer update for BidderMSP  ##########"
-./bin/configtxgen -profile BidChannel -configPath . -outputAnchorPeersUpdate ./BidderMSPanchors_bidchannel.tx -channelID $CHANNEL_NAME2 -asOrg BidderMSP
+echo "#######    Generating anchor peer update for Org1MSP  ##########"
+./bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./BidderMSPanchors_basicchannel.tx -channelID $CHANNEL_NAME2 -asOrg BidderMSP
+
+echo "#######    Generating anchor peer update for Org2MSP  ##########"
+./bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./GovMSPanchors_basicchannel.tx -channelID $CHANNEL_NAME2 -asOrg GovMSP
+
+echo "#######    Generating anchor peer update for Org3MSP  ##########"
+./bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./BankMSPanchors_basicchannel.tx -channelID $CHANNEL_NAME2 -asOrg BankMSP
--- createChannel.sh
+++ createChannel.sh
@@ -1,11 +1,12 @@
 export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/tendersys.com/orderers/orderer.tendersys.com/msp/tlscacerts/tlsca.tendersys.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/gov.tendersys.com/peers/peer0.gov.tendersys.com/tls/ca.crt
-export PEER0_ORG2_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bidder.tendersys.com/peers/peer0.bidder.tendersys.com/tls/ca.crt
-export PEER0_ORG3_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bank.tendersys.com/peers/peer0.bank.tendersys.com/tls/ca.crt
+export PEER0_ORG1_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bidder.tendersys.com/peers/peer0.bidder.tendersys.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/gov.tendersys.com/peers/peer0.gov.tendersys.com/tls/ca.crt
+# export PEER0_ORG3_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bank.tendersys.com/peers/peer0.bank.tendersys.com/tls/ca.crt
 export FABRIC_CFG_PATH=${PWD}/artifacts/channel/config/
 
-export CHANNEL_NAME=basicchannel
+export CHANNEL_NAME=bidchannel
+# export CHANNEL_NAME=basicchannel
 
 setGlobalsForOrderer(){
     export CORE_PEER_LOCALMSPID="OrdererMSP"
@@ -15,49 +16,33 @@ setGlobalsForOrderer(){
 }
 
 setGlobalsForPeer0Org1(){
-    export CORE_PEER_LOCALMSPID="GovMSP"
+    export CORE_PEER_LOCALMSPID="BidderMSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/gov.tendersys.com/users/Admin@gov.tendersys.com/msp
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bidder.tendersys.com/users/Admin@bidder.tendersys.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
 }
 
 setGlobalsForPeer1Org1(){
-    export CORE_PEER_LOCALMSPID="GovMSP"
+    export CORE_PEER_LOCALMSPID="BidderMSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/gov.tendersys.com/users/Admin@gov.tendersys.com/msp
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bidder.tendersys.com/users/Admin@bidder.tendersys.com/msp
     export CORE_PEER_ADDRESS=localhost:8051
     
 }
 
-setGlobalsForPeer2Org1(){
+setGlobalsForPeer0Org2(){
     export CORE_PEER_LOCALMSPID="GovMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/gov.tendersys.com/users/Admin@gov.tendersys.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
     
 }
 
-setGlobalsForPeer3Org1(){
+setGlobalsForPeer1Org2(){
     export CORE_PEER_LOCALMSPID="GovMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/gov.tendersys.com/users/Admin@gov.tendersys.com/msp
     export CORE_PEER_ADDRESS=localhost:10051
-    
-}
-
-setGlobalsForPeer0Org2(){
-    export CORE_PEER_LOCALMSPID="BidderMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bidder.tendersys.com/users/Admin@bidder.tendersys.com/msp
-    export CORE_PEER_ADDRESS=localhost:11051
-    
-}
-
-setGlobalsForPeer1Org2(){
-    export CORE_PEER_LOCALMSPID="BidderMSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bidder.tendersys.com/users/Admin@bidder.tendersys.com/msp
-    export CORE_PEER_ADDRESS=localhost:12051
     
 }
 
@@ -65,13 +50,13 @@ setGlobalsForPeer0Org3(){
     export CORE_PEER_LOCALMSPID="BankMSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/bank.tendersys.com/users/Admin@bank.tendersys.com/msp
-    export CORE_PEER_ADDRESS=localhost:13051
-    
+    export CORE_PEER_ADDRESS=localhost:11051
 }
 
 createChannel(){
     # rm -rf ./channel-artifacts/*
     setGlobalsForPeer0Org1
+    
     ./artifacts/channel/bin/peer channel create -o localhost:7050 -c $CHANNEL_NAME \
     --ordererTLSHostnameOverride orderer.tendersys.com \
     -f ./artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
@@ -93,34 +78,38 @@ joinChannel(){
     setGlobalsForPeer1Org1
     ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
-    setGlobalsForPeer2Org1
-    ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
-    setGlobalsForPeer3Org1
-    ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
     setGlobalsForPeer0Org2
     ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
     setGlobalsForPeer1Org2
     ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
 
-    setGlobalsForPeer0Org3
-    ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    # setGlobalsForPeer0Org3
+    # ./artifacts/channel/bin/peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
 }
 
 updateAnchorPeers(){
+
     setGlobalsForPeer0Org1
-    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    ./artifacts/channel/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/BidderMSPanchors_bidchannel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
     
     setGlobalsForPeer0Org2
-    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    ./artifacts/channel/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/GovMSPanchors_bidchannel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
     
+
+    # setGlobalsForPeer0Org1
+    # ./artifacts/channel/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/BidderMSPanchors_basicchannel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    
+    # setGlobalsForPeer0Org2
+    # ./artifacts/channel/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/GovMSPanchors_basicchannel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    
+    # setGlobalsForPeer0Org3
+    # ./artifacts/channel/bin/peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.tendersys.com -c $CHANNEL_NAME -f ./artifacts/channel/BankMSPanchors_basicchannel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
 }
 
 # removeOldCrypto
 
-# createChannel
+createChannel
 joinChannel
-# updateAnchorPeers
+updateAnchorPeers
